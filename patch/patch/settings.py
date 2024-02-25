0c5b5320627b4c12bd5c6867f3ed13d6481f928f
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-02-25 06:04:50 +0530

    model created

--- patch/patch/settings.py
+++ patch/patch/settings.py
@@ -15,7 +15,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'web',
+    # apps
+    'web',
 ]
 
 MIDDLEWARE = [
--- patch/web/models.py
+++ patch/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonials(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    designation = models.CharField(max_length=255)
