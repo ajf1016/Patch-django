17851db4ec90ca72546c5319eb1e9a799e62d3be
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-03-12 03:54:47 +0530

    register promoter model in admin

--- patch/web/admin.py
+++ patch/web/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from web.models import Testimonial
+from web.models import Testimonial, Promoter
 
 
 class TestimonialAdmin(admin.ModelAdmin):
-    list_display = ('name',  'designation','description')
-    # for showing datas on admin 
-    
-admin.site.register(Testimonial,TestimonialAdmin)
+    list_display = ('id', 'name',  'designation', 'description')
+    # for showing datas on admin
+
+
+admin.site.register(Testimonial, TestimonialAdmin)
+
+
+class PromoterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',  'image')
+    # for showing datas on admin
+
+
+admin.site.register(Promoter, PromoterAdmin)
