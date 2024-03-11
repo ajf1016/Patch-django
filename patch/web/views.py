9c889358f2ff0f41b1360e22c60ffb3058cdf96e
ajf1016 <83690916+ajf1016@users.noreply.github.com>
2024-03-12 03:57:20 +0530

    collecting data in views

--- patch/web/views.py
+++ patch/web/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from web.models import Testimonial
+from web.models import Testimonial,Promoter
 
 def index(request):
     testimonials = Testimonial.objects.all()
+    promoters = Promoter.objects.all()
+    
+    print(promoters)
     
     context = {
-        'testimonials': testimonials
+        'testimonials': testimonials,
+        'promoters' : promoters
     }
     return render(request,"index.html",context=context)
